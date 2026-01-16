79f669aa6f1ff385c9b51d579ef6e4236e702102
Md. Ashikujjaman Nur <43537056+ashiknur@users.noreply.github.com>
2026-01-16 23:19:13 +0600

    Revert "Ashik workstation"

--- LineFollowingRobotSimulator/Math.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cmath>
-
-inline float deg2rad(float d)
-{
-    return d * 3.1415926f / 180.f;
-}
--- LineFollowingRobotSimulator/config.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-constexpr unsigned WIDTH = 700;
-constexpr unsigned HEIGHT = 700;
-constexpr int THRESHOLD = 120;
-
-constexpr float LINE_WIDTH = 8.f;
